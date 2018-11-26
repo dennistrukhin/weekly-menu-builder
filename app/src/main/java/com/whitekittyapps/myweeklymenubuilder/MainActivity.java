8bef3c4996c34c91c79ba5d33d13629c28bd9b18
Denis Turkhin <dtrukhin@mercury.ru>
2018-11-26 17:20:36 +0300

    Auth

--- app/src/main/java/com/whitekittyapps/myweeklymenubuilder/MainActivity.java
+++ app/src/main/java/com/whitekittyapps/myweeklymenubuilder/MainActivity.java
@@ -1,13 +1,56 @@
 package com.whitekittyapps.myweeklymenubuilder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),"Main activity started", Toast.LENGTH_LONG).show();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user == null){
+                    startActivity(new Intent(getApplicationContext(), AuthActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        final FirebaseUser user  = firebaseAuth.getCurrentUser();
+//        Toast.makeText(getApplicationContext(),"Hi " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener!=null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
