3188252e21b368096b80cff6cfedfd3692de6b7e
Denis Turkhin <dtrukhin@mercury.ru>
2018-11-28 17:00:43 +0300

    info message in main activity

--- app/src/main/java/com/whitekittyapps/myweeklymenubuilder/MainActivity.java
+++ app/src/main/java/com/whitekittyapps/myweeklymenubuilder/MainActivity.java
@@ -3,6 +3,7 @@ package com.whitekittyapps.myweeklymenubuilder;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.TextView;
 
 import com.whitekittyapps.myweeklymenubuilder.Listeners.AddWeekButtonClickListener;
 
@@ -17,5 +18,10 @@ public class MainActivity extends BaseAuthorizedActivity {
 
         FloatingActionButton btnCreateWeek = findViewById(R.id.btn_add_week);
         btnCreateWeek.setOnClickListener(new AddWeekButtonClickListener(ctx));
+
+        if (user == null) {
+            return;
+        }
+        ((TextView)findViewById(R.id.list_weeks_welcome)).setText(String.format("Hi %s!", user.getDisplayName()));
     }
 }
